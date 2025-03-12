5ff4fb0e6b3f5d59d323d66a7e65aea9f23abff1
yoshi-nakane0 <yos242012@yahoo.co.jp>
2025-03-12 12:01:32 +0900

    update

--- build_files.sh
+++ build_files.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 # staticfilesディレクトリを作成
 mkdir -p staticfiles
-# manage.pyがあるディレクトリに移動
-cd ./myproject
+# ルートディレクトリにいることを確認（cd不要）
 # 静的ファイルを収集
-python manage.py collectstatic --noinput
+python3 manage.py collectstatic --noinput || python manage.py collectstatic --noinput
