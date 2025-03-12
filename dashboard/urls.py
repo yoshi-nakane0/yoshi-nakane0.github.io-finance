8da01299f39c9d310f31e5858996b6a27d762597
yoshi-nakane0 <yos242012@yahoo.co.jp>
2025-03-12 17:17:35 +0900

    dashboard

---
+++ dashboard/urls.py
@@ -0,0 +1,8 @@
+# dashboard/urls.py
+
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.index, name='index'),
+]
--- dashboard/views.py
+++ dashboard/views.py
@@ -1,4 +1,4 @@
-from django.http import HttpResponse
+from django.shortcuts import render
 
-def hello_world(request):
-    return HttpResponse("Hello, world!")
+def index(request):
+    return render(request, 'dashboard/index.html')
