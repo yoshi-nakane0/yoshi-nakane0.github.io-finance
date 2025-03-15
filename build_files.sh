3d1233aabda48f1be4e4aec36d5fee9a915bfbcc
yoshi-nakane0 <yos242012@yahoo.co.jp>
2025-03-15 22:25:37 +0900

    package

--- build_files.sh
+++ build_files.sh
@@ -1,5 +1,13 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
+# Install required packages
+pip install -r requirements.txt
+
+# Create staticfiles directory if it doesn't exist
+mkdir -p staticfiles/dashboard/css
+
+# Build Tailwind CSS
 npx tailwindcss -i ./dashboard/static/dashboard/css/style.css -o ./staticfiles/dashboard/css/style.css
 
+# Run collectstatic
 python manage.py collectstatic --noinput
