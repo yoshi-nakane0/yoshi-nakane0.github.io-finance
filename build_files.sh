c652c60225276d7357c4333e2343820cdc4e0860
yoshi-nakane0 <yos242012@yahoo.co.jp>
2025-03-18 17:24:48 +0900

    build3

--- build_files.sh
+++ build_files.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-pip3.9 install -r requirements.txt
-python3.9 manage.py collectstatic --noinput
+pip install -r requirements.txt
+python manage.py collectstatic --noinput
 cp -r myproject/static/* staticfiles/
