43befaa3ac7d71afac1d617e68afa1797588612b
yoshi-nakane0 <yos242012@yahoo.co.jp>
2025-03-18 21:24:37 +0900

    grok

--- build_files.sh
+++ build_files.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
+
+# pip の準備とアップグレード
 python3.9 -m ensurepip
-python3.9 -m pip install -r requirements.txt
+python3.9 -m pip install --upgrade pip --no-warn-script-location
+
+# 依存関係のインストール
+python3.9 -m pip install -r requirements.txt --no-warn-script-location
+
+# 静的ファイルの収集
 python3.9 manage.py collectstatic --noinput
+
+# 静的ファイルを staticfiles にコピー
 cp -r static/* staticfiles/
