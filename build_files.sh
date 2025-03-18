681b72b5dc84d7b00526e8665d8a7eec3d1b1347
yoshi-nakane0 <yos242012@yahoo.co.jp>
2025-03-18 20:28:28 +0900

    build

--- build_files.sh
+++ build_files.sh
@@ -1,16 +1,5 @@
 #!/bin/bash
-# Exit on error
-set -e
-
-# Create and activate virtual environment
-python -m venv venv
-source venv/bin/activate
-
-# Upgrade pip in virtual environment
-python -m pip install --upgrade pip
-
-# Install requirements
-pip install -r requirements.txt
-
-# Collect static files
-python manage.py collectstatic --noinput
+python3.9 -m ensurepip
+python3.9 -m pip install -r requirements.txt
+python3.9 manage.py collectstatic --noinput
+cp -r static/* staticfiles/
