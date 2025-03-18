f8b6af1468996ff79a7209e63a750afb7ea5b8f2
yoshi-nakane0 <yos242012@yahoo.co.jp>
2025-03-18 18:12:18 +0900

    build

--- build_files.sh
+++ build_files.sh
@@ -1,18 +1,4 @@
 #!/bin/bash
-# Vercel環境ではPythonとpipのパスを正確に指定
-export PATH=$PATH:/opt/buildhome/.python3/bin
-
-# パッケージのインストール
-pip3 install -r requirements.txt
-
-# 静的ファイルの収集
-python3 manage.py collectstatic --noinput
-
-# 静的ファイルのディレクトリ確認と移動
-# プロジェクト名に合わせて調整が必要
-if [ -d "myproject/static" ]; then
-    mkdir -p staticfiles
-    cp -r myproject/static/* staticfiles/
-else
-    echo "静的ファイルのディレクトリが見つかりません。パスを確認してください。"
-fi
+pip3.9 install -r requirements.txt
+python3.9 manage.py collectstatic --noinput
+cp -r static/* staticfiles/
